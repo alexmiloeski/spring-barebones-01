150555975c96789ea40765c8b3074ca56e41ac0a
alexmiloeski <a.miloeski@gmail.com>
2024-11-12 13:12:05 +0100

    add env var and secret that gets printed and returned in response

--- src/main/java/org/example/springbarebones01/Controller.java
+++ src/main/java/org/example/springbarebones01/Controller.java
@@ -1,14 +1,29 @@
 package org.example.springbarebones01;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+
 @RestController
 public class Controller {
+
+    @Value("${app.var1}")
+    private String var1;
+
+    @Value("${app.secret1}")
+    private String secret1;
+
     @GetMapping("/hello")
-    public String hello() {
+    public HashMap<String, String> hello() {
         System.out.println("Controller.hello");
-        return "hello";
+        System.out.println("var1 = " + var1);
+        System.out.println("secret1 = " + secret1);
+        HashMap<String, String> map = new HashMap<>();
+        map.put("var1", var1);
+        map.put("secret1", secret1);
+        return map;
     }
 
     @GetMapping("/bye")
